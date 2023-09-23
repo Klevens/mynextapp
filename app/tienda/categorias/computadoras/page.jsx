98a5ff1d384fdcbbc2f9f08b277b7036351e45ca
Klevens <kelvinreyescnh@gmail.com>
2023-09-23 17:37:12 -0400

    some changes

---
+++ app/tienda/categorias/computadoras/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ComputadorasPage() {
+  return (
+    <div>
+        <h1>ComputadorasPage</h1>
+    </div>
+  )
+}
---
+++ app/tienda/layout.jsx
@@ -0,0 +1,7 @@
+export default function TiendaLayout({children}) {
+    return (
+        <>
+        {children}
+        </>
+    )
+}
